ff884cea1af55850c77e74cd43dc2ef5df0a009a
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-04 08:36:33 +0300

    Initial commit

--- HW3.5.3.cpp
+++ HW3.5.3.cpp
@@ -1,20 +1,415 @@
-﻿// HW3.5.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+class Figure
+{
+protected:
+	std::string nameOfFigure_ = "Фигура";
+	bool checkFigure = false;
+	int quantityOfSides = 0;
+	int sideLengthA_ = 0;
+	int sideLengthB_ = 0;
+	int sideLengthC_ = 0;
+	int sideLengthD_ = 0;
+	int angleA_ = 0;
+	int angleB_ = 0;
+	int angleC_ = 0;
+	int angleD_ = 0;
+
+	virtual bool metodCheckFigure() //Виртуальный метод проверки фигуры.Кол-во сторон == 0 
+	{
+		if (quantityOfSides == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+public:
+	//Геттеры
+	std::string getNameOfFigure() { return nameOfFigure_; }
+
+	int getSideLengthA() { return sideLengthA_; }
+	int getSideLengthB() { return sideLengthB_; }
+	int getSideLengthC() { return sideLengthC_; }
+	int getSideLengthD() { return sideLengthD_; }
+
+	int getAngleA() { return angleA_; }
+	int getAngleB() { return angleB_; }
+	int getAngleC() { return angleC_; }
+	int getAngleD() { return angleD_; }
+
+	virtual void printFigure() //Виртуальный метод печати и проверка правильности фигуры
+	{
+		std::cout << std::endl;
+		std::cout << nameOfFigure_ << ":" << std::endl;
+		if (metodCheckFigure())
+		{
+			std::cout << "Правильная" << std::endl;
+		}
+		else { std::cout << "Неправильная" << std::endl; }
+
+		std::cout << "Количество сторон: " << quantityOfSides << std::endl;
+	}
+};
+
+class Triangle : public Figure
+{
+protected:
+	bool metodCheckFigure() override //Переписанный метод проверки на сумма всех углов треуг == 180
+	{
+		if ((angleA_ + angleB_ + angleC_) == 180)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+public:
+	//Конструктор треугольника
+	Triangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB, int angleC)
+	{
+		nameOfFigure_ = "Треугольник";
+		sideLengthA_ = sideLengthA;
+		sideLengthB_ = sideLengthB;
+		sideLengthC_ = sideLengthC;
+		angleA_ = angleA;
+		angleB_ = angleB;
+		angleC_ = angleC;
+		quantityOfSides = 3;
+		checkFigure = metodCheckFigure();
+	}
+
+	void printFigure() override //Переписанный метод печати с вызовом базового метода печати
+	{
+		Figure::printFigure();
+
+		std::cout << "Стороны:";
+		std::cout << " a = " << getSideLengthA() << ", b = " << getSideLengthB() << ", с = " << getSideLengthC();
+		std::cout << std::endl;
+
+		std::cout << "Углы:";
+		std::cout << " А = " << getAngleA() << ", В = " << getAngleB() << ", С = " << getAngleC();
+		std::cout << std::endl;
+	}
+
+};
+
+class rightTriangle : public Triangle
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Triangle::metodCheckFigure() && (angleC_ == 90))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	};
+
+public:
+	rightTriangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB, int angleC)
+		: Triangle(sideLengthA, sideLengthB, sideLengthC, angleA, angleB, angleC)
+	{
+		Triangle::nameOfFigure_ = "Прямоугольный треугольник";
+	};
+};
+
+class isoscelesTriangle : public Triangle
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Triangle::metodCheckFigure() && (sideLengthA_ == sideLengthC_ && angleA_ == angleC_))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	};
+
+public:
+	isoscelesTriangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB, int angleC)
+		: Triangle(sideLengthA, sideLengthB, sideLengthC, angleA, angleB, angleC)
+	{
+		Triangle::nameOfFigure_ = "Равнобедренный треугольник";
+	};
+};
+
+class equilateralTriangle : public Triangle
+{
+private:
+	bool CheckEquilateralTriangle()
+	{
+		if ((sideLengthA_ == sideLengthB_ && sideLengthB_ == sideLengthC_) && ((angleA_ == 60) && (angleA_ = 60) && (angleC_ = 60)))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	}
+
+public:
+	equilateralTriangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB, int angleC)
+		: Triangle(sideLengthA, sideLengthB, sideLengthC, angleA, angleB, angleC)
+	{
+		Triangle::nameOfFigure_ = "Равносторонний треугольник";
+	};
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Triangle::metodCheckFigure() && (CheckEquilateralTriangle()))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	};
+
+
+
+};
+
+class Quadrilateral : public Figure
+{
+protected:
+	virtual bool metodCheckFigure()
+	{
+		if ((angleA_ + angleB_ + angleC_ + angleD_) == 360)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+	
+	bool metodCheckSideLengthACiBD()
+	{
+		if (sideLengthA_ == sideLengthC_ && sideLengthB_ == sideLengthD_)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+	
+	bool metodCheckSideLengthACBD()
+	{
+		if (sideLengthA_ == sideLengthB_ && sideLengthB_ == sideLengthC_ && sideLengthC_ == sideLengthD_)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+	
+	bool metodCheckAngleabcd90()
+	{
+		if (angleA_ == 90 && angleB_ == 90 && angleC_ == 90 && angleD_ == 90)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+	
+	bool metodCheckAngleACiBD()
+	{
+		if (angleA_ == angleC_ && angleB_ == angleD_)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+public:
+	Quadrilateral(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
+	{
+		nameOfFigure_ = "Четырехугольник";
+
+		quantityOfSides = 4;
+		sideLengthA_ = sideLengthA;
+		sideLengthB_ = sideLengthB;
+		sideLengthC_ = sideLengthC;
+		sideLengthD_ = sideLengthD;
+
+		angleA_ = angleA;
+		angleB_ = angleB;
+		angleC_ = angleC;
+		angleD_ = angleD;
+	}
+
+	void printFigure() override
+	{
+		Figure::printFigure();
+
+		std::cout << "Стороны:";
+		std::cout << " a = " << getSideLengthA() << ", b = " << getSideLengthB() << ", с = " << getSideLengthC();
+		std::cout << ", d = " << getSideLengthD() << std::endl;
+
+		std::cout << "Углы:";
+		std::cout << " А = " << getAngleA() << ", В = " << getAngleB() << ", С = " << getAngleC();
+		std::cout << ", D = " << getAngleD() << std::endl;
+	}
+
+};
+
+class Rectangle : public Quadrilateral
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Quadrilateral::metodCheckFigure() && Quadrilateral::metodCheckSideLengthACiBD() && Quadrilateral::metodCheckAngleabcd90())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+public:
+	Rectangle(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
+		: Quadrilateral(sideLengthA, sideLengthB, sideLengthC, sideLengthD, angleA, angleB, angleC, angleD)
+	{
+		Quadrilateral::nameOfFigure_ = "Прямоугольник";
+	}
+};
+
+class Square : public Quadrilateral
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Quadrilateral::metodCheckFigure() && Quadrilateral::metodCheckSideLengthACBD() && Quadrilateral::metodCheckAngleabcd90())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+public:
+	Square(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
+		: Quadrilateral(sideLengthA, sideLengthB, sideLengthC, sideLengthD, angleA, angleB, angleC, angleD)
+	{
+		Quadrilateral::nameOfFigure_ = "Квадрат";
+	}
+};
+
+class Parallelogram : public Quadrilateral
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Quadrilateral::metodCheckFigure() && Quadrilateral::metodCheckSideLengthACiBD() && Quadrilateral::metodCheckAngleACiBD())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+public:
+	Parallelogram(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
+		: Quadrilateral(sideLengthA, sideLengthB, sideLengthC, sideLengthD, angleA, angleB, angleC, angleD)
+	{
+		Quadrilateral::nameOfFigure_ = "Параллелограмм";
+	}
+};
+
+class Rhomb : public Quadrilateral
+{
+protected:
+	bool metodCheckFigure() override
+	{
+		if (Quadrilateral::metodCheckFigure() && Quadrilateral::metodCheckSideLengthACBD() && Quadrilateral::metodCheckAngleACiBD())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+public:
+	Rhomb(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
+		: Quadrilateral(sideLengthA, sideLengthB, sideLengthC, sideLengthD, angleA, angleB, angleC, angleD)
+	{
+		Quadrilateral::nameOfFigure_ = "Ромб";
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+
+	Figure Figure1;
+	Figure1.printFigure();
+
+	Triangle Triangle1(10, 20, 30, 50, 60, 70);
+	Triangle1.printFigure();
+
+	rightTriangle rightTriangle1(10, 20, 30, 50, 60, 90);
+	rightTriangle1.printFigure();
+
+	rightTriangle rightTriangle2(10, 20, 30, 50, 40, 90);
+	rightTriangle2.printFigure();
+
+	isoscelesTriangle isoscelesTriangle1(10, 20, 10, 50, 60, 90);
+	isoscelesTriangle1.printFigure();
+
+	equilateralTriangle equilateralTriangle1(30, 30, 30, 60, 60, 60);
+	equilateralTriangle1.printFigure();
+
+	Quadrilateral Quadrilateral1(10, 20, 30, 40, 50, 60, 70, 80);
+	Quadrilateral1.printFigure();
+
+	Rectangle Rectangle1(10, 20, 10, 20, 90, 90, 90, 90);
+	Rectangle1.printFigure();
+
+	Square Square1(20, 20, 20, 20, 90, 90, 90, 90);
+	Square1.printFigure();
+
+	Parallelogram Parallelogram1(20, 30, 20, 30, 30, 40, 30, 40);
+	Parallelogram1.printFigure();
+
+	Rhomb Rhomb1(30, 30, 30, 30, 30, 40, 30, 40);
+	Rhomb1.printFigure();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
